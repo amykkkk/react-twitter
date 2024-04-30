bf47b6d3883cc57d67f9ecfb3c4f1edd83d192af
kimayeong <kimayeong@valofe.com>
2024-04-30 16:48:58 +0900

    Fetching Timeline

---
+++ src/components/timeline.tsx
@@ -0,0 +1,50 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { db } from "../firebase";
+import Tweet from "./tweet";
+
+export interface ITweet {
+  id: string;
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+export default function Timeline() {
+  const [tweets, setTweet] = useState<ITweet[]>([]);
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createdAt", "desc")
+    );
+    const spanshot = await getDocs(tweetsQuery); //쿼리 실행
+    const tweets = spanshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id, // id는 바깥위치에 존재
+      };
+    });
+    setTweet(tweets);
+  };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return (
+    <Wrapper>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
